503af9c134abfff5343df3dc1e5d2c9f3dd56028
venkatamaheshwara <entenkivenkatamaheshwara@gmail.com>
2024-03-17 16:46:29 +0530

    introduced subtraction function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use test_automation::add;
+use test_automation::sub;
 fn main() {
     println!("Hello, world!");
     let a=10;
     let b=20;
     let c=add(a,b);
+    let d=sub(a,b);
     println!("addition of {a} and {b} = {c}");
+    println!("subtraction of {a} and {b} = {d}");
 }
